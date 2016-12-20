0175e66d786e62919aceb784823524833d568bab
João Victor Sousa <victtorss09@gmail.com>
2016-12-20 17:56:51 -0200

    Add files via upload

---
+++ Estudos/Estudos/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estudos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("informe o primeiro valor? ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("informe o primeiro valor? ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Soma é:" + (num1 + num2));
+
+
+            Console.WriteLine("Deseja Continuar ? 1- SIM  2 - NÃO");
+            int rep = int.Parse(Console.ReadLine());
+            while(rep==1)
+            {
+                Console.WriteLine("informe o primeiro valor? ");
+                num1 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("informe o primeiro valor? ");
+                num2 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Soma é:" + (num1 + num2));
+            }
+
+
+            
+            }
+
+        }
+    }
+
+
